50da4161578f94e9db10048afb94c86917e82928
Tul <tul3006@gmail.com>
2025-04-07 17:39:33 +0100

    automated testing using python test files and pytest

--- scripts/test_directories.sh
+++ scripts/test_directories.sh
@@ -1,8 +1,4 @@
-# git ls-tree -t HEAD:./ | awk '{if ($2 == "tree") print $4;}' | grep -e '^[a-zA-Z]' > all_dir.txt
-
 git ls-files | grep -e '^[a-zA-Z]*/test_[a-z]*\.py' > all_test_file_paths.txt
-
-cat all_test_file_paths.txt
 
 echo pytesting all python files in every directory
 
@@ -10,11 +6,9 @@ num_of_dir=$(wc -l all_test_file_paths.txt | awk ' { print $1 } ')
 
 for i in $(seq 1 ${num_of_dir});
 do
-    dir_in_repo=$(sed "${i}q;d" all_test_file_paths.txt)
-    pytest ${dir_in_repo} -x -vv
+    test_file_in_repo=$(sed "${i}q;d" all_test_file_paths.txt)
+    echo "Testing file from this path: $test_file_in_repo"
+    pytest ${test_file_in_repo} -x -vv
 done
-
-echo pytesting rest of python files in main repo
-pytest . 
 
 echo "All files tested"

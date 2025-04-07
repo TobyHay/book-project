576a14b7df288370989eb6ca53eb8f5c1b108e3a
Rodrigo Montemayor <trainee.rodrigo.montemayor@sigmalabs.co.uk>
2025-04-07 15:31:53 +0100

    Added basic beautifulsoup scraping to get aggregate values from the authors page

--- extract-authors/extract.py
+++ extract-authors/extract.py
@@ -12,14 +12,22 @@ def get_author_page_parser(author_url:str) -> BeautifulSoup:
         html = page.read().decode('utf-8')
     return BeautifulSoup(html, "lxml")
 
-def get_user_info(author_soup:BeautifulSoup) -> dict:
+def get_author_name_from_url(url:str) -> str:
+    end_index = url.rfind('?')
+    start_index = url.rfind('.')+1
+    return url[start_index:end_index]
+
+def get_user_info(author_url:str) -> dict:
     '''Scrapes average_rating, rating_count and review_count
       for a given goodreads.com author url'''
+    author_soup:BeautifulSoup = get_author_page_parser(author_url)
     aggregate_contents = author_soup.find("div",class_="hreview-aggregate")
+
     average_rating = aggregate_contents.find("span",class_="average").text
     rating_count = aggregate_contents.find("span",class_="votes").text.strip()
     review_count = aggregate_contents.find("span",class_="count").text.strip()
     return {
+        'author_page':author_url,
         'average_rating':average_rating,
         'rating_count':rating_count,
         'review_count':review_count

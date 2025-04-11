506bde19a2c97ef3aa01033f37620ae5358723b8
johnadibpour@gmail.com <johnadibpour@gmail.com>
2025-04-11 18:18:49 +0100

    linted

--- dashboard/welcome.py
+++ dashboard/welcome.py
@@ -1,11 +1,9 @@
 '''A script that creates a Streamlit dashboard using book data from the RDS'''
 import os
-from datetime import datetime, timedelta
 from dotenv import load_dotenv
 import pandas as pd
 import requests
 import streamlit as st
-import altair as alt
 import psycopg2
 
 
@@ -63,91 +61,110 @@ def pipeline(author_url: str, conn: psycopg2.connect) -> bool:
     pass
 
 
-def streamlit(conn: psycopg2.connect) -> None:
-    st.title(":books: Book and Author Tracker - Publisher Dashboard! :books:")
-    st.write("Welcome to the Publisher Dashboard! On this page, you'll be able to request authors to be tracked and submit email report preferences.")
-    st.write("On the next page, summary statistics for some of our most popular authors can be seen. Use our interactive features to learn about your favourite author!")
+def infer_author_name(url: str) -> str:
+    '''Attempts to extract author name from URL for display purposes'''
+    try:
+        author_name = " ".join(url.split(".")[-1].split("_"))
+        if not author_name.strip():
+            raise ValueError("Unknown Author")
+        return author_name
+    except Exception:
+        return "Unknown Author"
+
+
+def validate_email(email: str) -> bool:
+    '''Basic validation checks on user-submitted email address'''
+    if "@" not in email:
+        return False
+    if "." not in email:
+        return False
+    return True
+
+
+def summary_cols(conn: psycopg2.connect) -> None:
+    '''Simple Streamlit summary statistics'''
     col1, col2, col3 = st.columns(3)
+
     with col1:
+
         books = len(pd.read_sql("SELECT * FROM book;", conn))
         books_since_yesterday = len(pd.read_sql(
             "SELECT * FROM book WHERE date_added >= DATE_TRUNC('day', NOW() - INTERVAL '1 day');", conn))
+
         st.metric("Number of books registered in our database",
                   books, f"{books_since_yesterday} books since yesterday", border=True)
+
     with col2:
+
         authors = len(pd.read_sql("SELECT * FROM author;", conn))
         authors_since_yesterday = len(pd.read_sql(
             "SELECT * FROM author WHERE date_added >= DATE_TRUNC('day', NOW() - INTERVAL '1 day');", conn))
+
         st.metric("Number of authors signed up for tracking",
                   authors, f"{authors_since_yesterday} authors since yesterday", border=True)
+
     with col3:
+
         publishers = len(pd.read_sql("SELECT * FROM publisher;", conn))
         publishers_since_yesterday = len(pd.read_sql(
             "SELECT * FROM publisher WHERE date_subscribed >= DATE_TRUNC('day', NOW() - INTERVAL '1 day');", conn))
+
         st.metric("Number of publishers using the Book and Author Tracker",
                   publishers, f"{publishers_since_yesterday} publishers since yesterday", border=True)
-    st.header("Are you a bookworm? :book: :worm:")
-    st.write("Request your favourite book or author to be added to our database! Authors or books added will be included in our summary statistics on the next page.")
 
-    with st.form("Author request form"):
-        st.write("Please paste your chosen author's URL from GoodReads.com, in the following format: https://www.goodreads.com/author/show/153394.Suzanne_Collins Please note that this page must be the overall author page; .../author/show/... should be in the URL.")
-        author_url_input = st.text_input(
-            "Paste an author's URL here:", key="Author URL input")
-        submitted_author_request = st.form_submit_button(
-            'Add this author to the database')
 
-        if submitted_author_request:
-            try:
-                author_name = " ".join(
-                    author_url_input.split(".")[-1].split("_"))
-            except Exception:
-                author_name = "Unknown Author"
-            # Change author name
-            finally:
+def author_request_form(conn: psycopg2.connect) -> None:
+    '''Form for requesting author via URL, triggers ETL pipeline'''
+    st.write("Please paste your chosen author's URL from GoodReads.com, in the following format: https://www.goodreads.com/author/show/153394.Suzanne_Collins Please note that this page must be the overall author page; .../author/show/... should be in the URL.")
+
+    author_url_input = st.text_input(
+        "Paste an author's URL here:", key="Author URL input")
+
+    submitted_author_request = st.form_submit_button(
+        'Add this author to the database')
+
+    if submitted_author_request:
+        author_name = infer_author_name(author_url_input)
+        st.write(
+            f":hourglass_flowing_sand: Adding {author_name} to the author database...")
+        if validate_author_url(author_url_input) is True:
+            if is_author_in_system(author_url_input, conn) is True:
+                st.write("Author is already in the system!")
+            else:
                 st.write(
-                    f":hourglass_flowing_sand: Adding {author_name} to the author database...")
-                if validate_author_url(author_url_input) == True:
-                    if is_author_in_system(author_url_input, conn) == True:
-                        st.write("Author is already in the system!")
-                    else:
-                        st.write(
-                            ":hourglass: Loading the author's most up-to-date information...")
-                        try:
-                            pipeline(author_url_input, conn)
-                            st.write(
-                                f":white_check_mark: Information loaded - head over to the visualisations page for in-depth analysis of {author_name}'s work!")
+                    ":hourglass: Loading the author's most up-to-date information...")
+                try:
+                    pipeline(author_url_input, conn)
+                    st.write(
+                        f":white_check_mark: Information loaded - head over to the visualisations page for in-depth analysis of {author_name}'s work!")
+                except Exception:
+                    st.write(
+                        ":x: Something went wrong when getting author's information - please try again, or contact the devs.")
+                # import pipeline script's function
 
-                        except Exception as e:
-                            st.write(
-                                ":x: Something went wrong when getting author's information - please try again, or contact the devs.")
 
-                    # Check if author already in DB
-                    # Check if GoodReads.com/URL requests.gets a valid 200 response. Otherwise, print ("Cannot find author with that URL. Double check or try another.")
-                    # ETL script taking in Author URL:
-                    # INSERTING INTO author VALUES author_url, author_name, author_image, (author_id and author_date_added would auto-generate)
-                    # INSERTING INTO book VALUES (all books associated with author)
-                    # INSERTING INTO author_measurement (single author_measurement at current time)
-                    #  INSERTING INTO book_measurement VALUES (single book_measurement for all books at current time)
-                    # import pipeline script's function
+def publisher_signup_form(conn: psycopg2.connect) -> None:
+    '''Form for publishers to sign up to author emails'''
+    st.write("Please enter your name, email, and the select one of our tracked authors from the drop-down menu. To subscribe to an email report for multiple authors, please submit the form once for each author URL.")
 
-    st.header("Calling all publishers! :lower_left_ballpoint_pen:")
-    st.write("We'll send you daily updates on authors of your choice - just enter your name, email, and the author you want to track, and we'll do the rest!")
-    with st.form("Publisher sign-up form"):
-        st.write("Please enter your name, email, and the select one of our tracked authors from the drop-down menu. To subscribe to an email report for multiple authors, please submit the form once for each author URL.")
-        publisher_name_input = st.text_input(
-            "Publisher name to receive updates:", key="Publisher name input")
-        publisher_name_input = publisher_name_input[:50]
-        publisher_email_input = st.text_input(
-            "Publisher email to receive updates:", key="Publisher email input")
-        publisher_email_input = publisher_email_input[:50]
+    publisher_name_input = st.text_input(
+        "Publisher name to receive updates:", key="Publisher name input")
+    publisher_name_input = publisher_name_input[:50]
 
-        list_of_authors = pd.read_sql("SELECT author_name FROM author;", conn)[
-            'author_name'].unique()
-        publisher_author_choice = st.selectbox(
-            "select author", list_of_authors)
-        submitted_publisher_submission = st.form_submit_button(
-            "Add this author to your mailing list")
-        if submitted_publisher_submission:
+    publisher_email_input = st.text_input(
+        "Publisher email to receive updates:", key="Publisher email input")
+    publisher_email_input = publisher_email_input[:50]
+
+    list_of_authors = pd.read_sql("SELECT author_name FROM author;", conn)[
+        'author_name'].unique()
+    publisher_author_choice = st.selectbox(
+        "select author", list_of_authors)
+
+    submitted_publisher_submission = st.form_submit_button(
+        "Add this author to your mailing list")
+
+    if submitted_publisher_submission:
+        if validate_email(publisher_email_input):
             cursor = conn.cursor()
             cursor.execute(
                 "INSERT INTO publisher (publisher_name, publisher_email) VALUES (%s, %s) RETURNING publisher_id", (publisher_name_input, publisher_email_input))
@@ -164,6 +181,30 @@ def streamlit(conn: psycopg2.connect) -> None:
                 f":white_check_mark: Successfully subscribed {publisher_name_input} ({publisher_email_input}) to {publisher_author_choice}! You will receive your first daily report tomorrow morning.")
 
 
-if __name__ == "__main__":
+def streamlit() -> None:
+    '''Main Streamlit script for the dashboard'''
     conn = get_connection()
-    streamlit(conn)
+
+    st.title(":books: Book and Author Tracker - Publisher Dashboard! :books:")
+    st.write("Welcome to the Publisher Dashboard! On this page, you'll be able to request authors to be tracked and submit email report preferences.")
+    st.write("On the next page, summary statistics for some of our most popular authors can be seen. Use our interactive features to learn about your favourite author!")
+
+    summary_cols(conn)
+
+    st.header("Are you a bookworm? :book: :worm:")
+    st.write("Request your favourite book or author to be added to our database! Authors or books added will be included in our summary statistics on the next page.")
+
+    with st.form("Author request form"):
+        author_request_form(conn)
+
+    st.header("Calling all publishers! :lower_left_ballpoint_pen:")
+    st.write("We'll send you daily updates on authors of your choice - just enter your name, email, and the author you want to track, and we'll do the rest!")
+
+    with st.form("Publisher sign-up form"):
+        publisher_signup_form(conn)
+
+    conn.close()
+
+
+if __name__ == "__main__":
+    streamlit()

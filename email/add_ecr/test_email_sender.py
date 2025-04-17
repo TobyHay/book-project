b38b215a168390c375ac11c12ed216e6a29a58ad
Rodrigo Montemayor <trainee.rodrigo.montemayor@sigmalabs.co.uk>
2025-04-17 12:30:46 +0100

    Changed tests based on tuls feedback

--- email/add_ecr/test_email_sender.py
+++ email/add_ecr/test_email_sender.py
@@ -1,3 +1,5 @@
+'''This script uses pytest to test email_sender.py'''
+# pylint: skip-file
 import pytest
 from unittest.mock import MagicMock, patch
 import psycopg2
@@ -6,6 +8,7 @@ from email_sender import get_avg_rating_difference_since_yesterday, get_db_conne
 
 @pytest.fixture(scope='module')
 def get_mock_conn() -> MagicMock:
+    '''Mocks a psycopg2 connect object with a mock cursor method.'''
     mock_conn = MagicMock()
     mock_cursor = MagicMock()
     mock_conn.cursor.return_value = mock_cursor
@@ -13,7 +16,9 @@ def get_mock_conn() -> MagicMock:
 
 
 @patch('email_sender.get_db_connection')
-def test_get_avg_rating_no_data(mock_get_db_connection, get_mock_conn):
+def test_get_avg_rating_no_data(mock_get_db_connection, get_mock_conn: MagicMock) -> None:
+    '''tests that the get_avg_rating_difference_since_yesterday returns the 
+    expected outcome if there's no data available for a given author.'''
     mock_get_db_connection.return_value = get_mock_conn
     get_mock_conn.cursor.return_value.fetchall.return_value = []
     assert get_avg_rating_difference_since_yesterday(
